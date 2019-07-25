2f750d1613d32b998b07239906f9e094757cbce5
nikashio <nikashio42@gmail.com>
2019-07-25 15:49:28 +0400

    TICKET-460 corrected mistakes

---
+++ app/code/Dev/ProductComment/Model/Attribute/Frontend/Comments.php
@@ -0,0 +1,11 @@
+<?php
+namespace Dev\ProductComment\Model\Attribute\Frontend;
+
+class Comments extends \Magento\Eav\Model\Entity\Attribute\Frontend\AbstractFrontend
+{
+    public function getValue(\Magento\Framework\DataObject $object)
+    {
+        $value = $object->getData($this->getAttribute()->getAttributeCode());
+        return "<b>$value</b>";
+    }
+}
